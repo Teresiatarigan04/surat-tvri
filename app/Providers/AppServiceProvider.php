bfbbd4b46d9005bf5b6cc4122f931e7cfe6359cb
Teresiatarigan04 <teresiatarigan557@gmail.com>
2026-06-17 00:14:45 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('path.public', function() {
+            // Jika ada folder public_html di luar folder project (seperti di shared hosting), gunakan itu.
+            // Jika tidak ada (seperti di lokal/komputer sendiri), gunakan folder public bawaan.
+            return is_dir(base_path('../public_html')) 
+                ? base_path('../public_html') 
+                : base_path('public');
+        });
     }
 
     /**
